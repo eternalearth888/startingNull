170cb941856010b55dc3e65906687c52a968b739
eternalearth888 <mdeslis@gmail.com>
2012-11-04 00:26:04 -0600

    basic ruby

--- Ruby/chapter1/helloworld.rb
+++ Ruby/chapter1/helloworld.rb
@@ -1,9 +1,22 @@
-# puts is a method that outputs a string
-puts 'hello world'
+#!/usr/bin/ruby
+#Hello World program
+	# puts is a method that outputs a string
+	puts 'hello world'
 
-#print allows the user to enter their name
-print( 'Enter your name:' )
-name = gets()
-#usr/bin/ruby env
+#Getting and Putting input
+	#print allows the user to enter their name
+	print( 'Enter your name:' )
+	name = gets()
+	puts( "Hello #{name}")
 
-puts( "Hello #{name}")
+#Strings and Embedded Evaluation
+	#try to find out what the output would be with the following line
+	puts( "\n\t#{(1+2) * 3}\nGoodbye" )
+
+	#start output
+	#
+	#	9
+	#Goodbye
+	#end output
+
+
