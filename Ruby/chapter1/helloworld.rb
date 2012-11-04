995886bb88603ce732ebba31cca3cea09b85f26d
eternalearth888 <mdeslis@gmail.com>
2012-11-04 00:47:09 -0600

    will finish the rest of chapter 1 for ruby in the morning

--- Ruby/chapter1/helloworld.rb
+++ Ruby/chapter1/helloworld.rb
@@ -15,6 +15,16 @@
 	print "Enter price: "
 	s = gets
 	subtotal = s.to_f
+
+#Testing a condition: if..then
+#	making sure to not accept a negative subtotal
+	if (subtotal < 0.0) then
+		subtotal=0.0
+	end
+
+#can be written like this: if (subtotal<0.0) then subtotal = 0.0 end
+#but it adds nothing to the clarity of the form
+
 	tax=subtotal*taxrate
 	puts "Tax on$#{subtotal} is $#{tax}, so grandtotal is $#{subtotal+tax}"
 
