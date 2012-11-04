6151a5ccdd2f81ea4ba985d1420d4eb045eb0dcf
eternalearth888 <mdeslis@gmail.com>
2012-11-04 00:34:49 -0600

    ruby numbers and comments

--- Ruby/chapter1/helloworld.rb
+++ Ruby/chapter1/helloworld.rb
@@ -1,22 +1,28 @@
 #!/usr/bin/ruby
 #Hello World program
-	# puts is a method that outputs a string
 	puts 'hello world'
 
 #Getting and Putting input
-	#print allows the user to enter their name
 	print( 'Enter your name:' )
 	name = gets()
 	puts( "Hello #{name}")
 
 #Strings and Embedded Evaluation
-	#try to find out what the output would be with the following line
 	puts( "\n\t#{(1+2) * 3}\nGoodbye" )
 
-	#start output
-	#
-	#	9
-	#Goodbye
-	#end output
+#Numbers
+	taxrate = 0.175
+	print "Enter price: "
+	s = gets
+	subtotal = s.to_f
+	tax=subtotal*taxrate
+	puts "Tax on$#{subtotal} is $#{tax}, so grandtotal is $#{subtotal+tax}"
 
+#Comments
+	# are for single lines
+=begin
+This is a
+multiline
+comment
+=end
 
